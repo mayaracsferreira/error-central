183abe46f0d9c71e829513e2e70f97cd8250e89a
Tabatha Macedo <33585625+tabskey@users.noreply.github.com>
2020-06-28 23:32:12 -0300

    Atualizado o modelo conforme a última aula e adicionado o model

---
+++ backend/ErrorCentral.WebAPI/ErrorCentral.Infrastructure/Context/EventContext.cs
@@ -0,0 +1,26 @@
+﻿using ErrorCentral.AppDomain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErrorCentral.Infrastructure.Context
+{
+    public class EventContext : DbContext
+    {
+        public DbSet<EventLog> EventLogs { get;set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //Essa parte, por favor, evitem rodar kkk
+                // A não ser que troque para o data Source do banco de vocês
+                //PRESTE ATENÇÃO, SUSCETIVEL A ACONTECER MERDA
+                optionsBuilder.UseSqlServer("Data Source=desktop-gdvu4gc\\sqlexpress;Initial Catalog=Carro;Integrated Security=True");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
