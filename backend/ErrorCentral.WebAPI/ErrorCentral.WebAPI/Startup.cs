f068ebba635859d58ddbd8209123c755cf367610
Mayara Cristina da Silva Ferreira <mcristina@img.com.br>
2020-07-05 15:19:51 -0300

    Adicionando Swagger a pagina inicial do projeto

--- backend/ErrorCentral.WebAPI/ErrorCentral.WebAPI/Startup.cs
+++ backend/ErrorCentral.WebAPI/ErrorCentral.WebAPI/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ErrorCentral.WebAPI
 {
@@ -32,7 +35,12 @@ namespace ErrorCentral.WebAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ErrorCentral", Version = "v1", Description = "ASP.NET Core Web API", });
-            });
+
+                var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name}.xml";                    
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+                //c.AddFluentValidationRules();
+            });            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
