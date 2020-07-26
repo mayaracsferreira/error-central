890baf8bd47c86495557274fa6853429805a3c0c
Tabatha Macedo <33585625+tabskey@users.noreply.github.com>
2020-07-25 21:46:42 -0300

    Atualização do banco

--- ErrorCentral.Infrastructure/Context/EventContext.cs
+++ ErrorCentral.Infrastructure/Context/EventContext.cs
@@ -19,7 +19,7 @@ namespace ErrorCentral.Infrastructure.Context
                 // A não ser que troque para o data Source do banco de vocês
                 //PRESTE ATENÇÃO, SUSCETIVEL A ACONTECER MERDA
                 //vou colocar o meu, desculpa se eu esquecer e subir com ele 
-                optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=error;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=tcp:errorcentralproject.database.windows.net,1433;Initial Catalog=ErrorCentral;Persist Security Info=False;User ID=admerrorcentral;Password=Squad1errorcentral;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
 
             base.OnConfiguring(optionsBuilder);
